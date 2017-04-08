5ff8cb207554381e4f5ed457ca2b0b94ba836ccb
campus <campus@S31911>
2017-04-08 12:41:33 +0200

    dgfd

--- WFMultiThreading/Form1.Designer.cs
+++ WFMultiThreading/Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.btn_second = new System.Windows.Forms.Button();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(442, 39);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(131, 23);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Main Button";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // rtb
+            // 
+            this.rtb.Location = new System.Drawing.Point(27, 28);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(381, 240);
+            this.rtb.TabIndex = 1;
+            this.rtb.Text = "";
+            // 
+            // btn_second
+            // 
+            this.btn_second.Location = new System.Drawing.Point(442, 80);
+            this.btn_second.Name = "btn_second";
+            this.btn_second.Size = new System.Drawing.Size(131, 23);
+            this.btn_second.TabIndex = 2;
+            this.btn_second.Text = "Button Second";
+            this.btn_second.UseVisualStyleBackColor = true;
+            this.btn_second.Click += new System.EventHandler(this.btn_second_Click);
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(27, 287);
+            this.txtBox.Multiline = true;
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(381, 56);
+            this.txtBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 396);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.btn_second);
+            this.Controls.Add(this.rtb);
+            this.Controls.Add(this.btn_start);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.Button btn_second;
+        private System.Windows.Forms.TextBox txtBox;
     }
 }
 
--- WFMultiThreading/Form1.cs
+++ WFMultiThreading/Form1.cs
@@ -3,18 +3,60 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WFMultiThreading
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class Form1 : Form
+	{
+		string pathNF = @"D:\new.log";
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btn_start_Click(object sender, EventArgs e)
+		{
+			var path = @"D:\dismc.log";
+			var lines = File.ReadAllLines(path);
+
+			new Thread(() =>
+			{
+				foreach (var line in lines)
+				{
+					foreach (char letter in line)
+					{
+						new Thread(() =>
+						{
+							rtb.BeginInvoke(new MethodInvoker(() =>
+								{
+									rtb.AppendText(letter.ToString());
+									Thread.Sleep(500);
+								}));
+						}).Start();
+					}
+				}
+			}).Start();
+
+		}
+
+
+		private void btn_second_Click(object sender, EventArgs e)
+		{
+			new Thread(() =>
+			{
+				var buffer = Encoding.UTF8.GetBytes(txtBox.Text);
+				rtb.BeginInvoke(new MethodInvoker(() =>
+				{
+					rtb.AppendText(Encoding.UTF8.GetString(buffer));
+					//Thread.Sleep(500);
+				}));
+			}).Start();
+		}
+	}
 }
